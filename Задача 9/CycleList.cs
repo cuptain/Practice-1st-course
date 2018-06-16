cd1d360ed052fa95cbfca67f490c2621ea6e3eae
Твой <vasvasal@gmail.com>
2018-06-17 04:26:19 +0500

    Девятая чудо-задача

---
+++ Задача 9/CycleList.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Задача_9
+{
+    class Point // Класс списка
+    {
+        public int data { get; set; } //информационное поле
+        public Point next { get; set; } //адресное поле
+
+        public Point(int Data) //Конструктор с параметрами
+        {
+            data = Data;
+            next = null;
+        }
+    }
+
+    class CycleList
+    {
+        public Point head { get; set; } // Начало списка
+
+        public Point tail { get; set; } //Конец списка
+
+        public CycleList() //Конструктор без параметров
+        {
+            head = null;
+            tail = null;
+        }
+
+        public void CreateCircularList(int N) //Конструктор с параметрами
+        {
+            Point head = new Point(N); //Создание начала
+
+            Point tail = new Point(1); //Создание конца
+
+            //Привязка конца к началу
+            head.next = tail;
+            tail.next = head;
+
+            Add(N - 1, head, tail);
+
+            this.head = tail.next;
+            this.tail = tail;
+        }
+
+        public static void Add(int N, Point head, Point tail) //Рекурсивное добавление элементов
+        {
+            if (N == 1)
+                return;
+            else
+            {
+                Point buf = new Point(N);
+                head.next = buf;
+                buf.next = tail;
+                Add(N - 1, buf, tail);
+            }
+        }
+
+        public Point Search(int value, Point curr, Point end) //Рекурсивный поиск элемента
+        {
+            if (curr.data == value || (curr.data == end.data && curr.next == end.next)) //Проверка на список из одного элемента
+            {
+                if (curr.data == value)
+                    return curr;
+                else
+                    return new Point(0);
+            }
+            else
+            {
+                Point wanted = Search(value, curr.next, end);
+                return wanted;
+            }
+        }
+
+        public Point Remove(int value, Point prev, Point curr, Point end) //Рекурсивное удаление элемента
+        {
+            if (curr.data == value || (curr.data == end.data && curr.next == end.next)) //Проверка на список из одного элемента
+            {
+                if (curr.data == value)
+                    prev.next = curr.next;
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("В списке нет элемента с введённым значением");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                return end;
+            }
+            else
+            {
+                Point deleted = Remove(value, prev.next, curr.next, end);
+                return deleted.next;
+            }
+        }
+
+        public void Show() //Вывод списка
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Point current = head;
+            do
+            {
+                Console.Write(current.data + " ");
+                current = current.next;
+            } while (current != head);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ Задача 9/Задача 9.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Задача_9
+{
+    class Program
+    {
+        //Ввод целых чисел
+        private static int Input(string task)
+        {
+            int number;
+            bool ok = false;
+            do
+            {
+                Console.WriteLine(task);
+                ok = int.TryParse(Console.ReadLine(), out number);
+                if (!ok)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ввод неправильный, нужно ввести целое число. Повторите попытку:\n");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            } while (!ok);
+            return number;
+        }
+
+        //Ввод числа в гранях
+        private static int ReadVGran(int min, int max, string task, string name = null)
+        {
+            int chislo;
+            do
+            {
+                chislo = Input(task);
+                if (chislo <= min || chislo >= max)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ошибка! " + name + " должен(-но) быть больше, чем {0} и меньше, чем {1}. Попробуйте ещё раз:\n", min, max);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            } while (chislo <= min || chislo >= max);
+            return chislo;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            int N = ReadVGran(0,Int32.MaxValue,"Введите количество элементов списка (N):", "Количество элементво списка");
+            Console.WriteLine("\nСозданный список: ");
+            CycleList cycleList = new CycleList();
+            cycleList.CreateCircularList(N);
+            cycleList.Show();
+            Console.WriteLine("\n");
+
+            int value = Input("Введите элемент, который хотите найти:");
+            Point wanted = cycleList.Search(value, cycleList.head, cycleList.tail);
+            if (wanted.next == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nВ списке нет элемента с введённым значением");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nНайденный элемент: {0}\nСледующий элемент: {1}", wanted.data, wanted.next.data);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine();
+            value = Input("Введите элемент, который хотите удалить из списка:");
+            cycleList.head = cycleList.Remove(value, cycleList.tail, cycleList.head, cycleList.tail);
+            Console.WriteLine("\nПолучившийся список:\n");
+            cycleList.Show();
+            Console.ReadKey();
+        }
+    }
+}
